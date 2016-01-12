9a7694813a380e168312af315598d9f8b5a12b24
Alberto Sierra <alberto.sierra@murciaeduca.es>
2016-01-12 12:55:18 +0100

    blueprints route-actions para preguntas

---
+++ api/controllers/PreguntaController.js
@@ -0,0 +1,11 @@
+/**
+ * PreguntaController
+ *
+ * @description :: Server-side logic for managing preguntas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

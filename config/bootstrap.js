fae46eda8cd0739d2bb84c2f828e703f3c3f4782
Alberto Sierra <alberto.sierra@murciaeduca.es>
2016-01-19 14:14:22 +0100

    Integrando fixtures

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,10 +8,14 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var fixtures = require('sails-fixtures');
 
 module.exports.bootstrap = function(cb) {
-
+  fixtures.init({
+    'dir':'/home/alumno/dataFixtures',
+    'pattern':'*.json' // Default is '*.json' 
+  },cb);
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  //cb();
 };

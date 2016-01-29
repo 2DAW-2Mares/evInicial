bee6e1917ac2e9e300dd8eec4f675319a837dab9
Alberto Sierra <alberto.sierra@murciaeduca.es>
2016-01-29 11:58:34 +0100

    Autenticando con google oauth2

--- config/passport.js
+++ config/passport.js
@@ -7,5 +7,16 @@ _.merge(exports, _super);
 _.merge(exports, {
 
   // Extend with custom logic here by adding additional fields, methods, etc.
-
+	passport: {
+		google : {
+		    name: 'Google',
+		    protocol: 'oauth2',
+		    strategy: require('passport-google-oauth').OAuth2Strategy,
+		    options: {
+		      clientID: '735582085260-82qpbj88q7dc0tog2efhomrkj69k8ahk.apps.googleusercontent.com',
+		      clientSecret: 'Md_kylqc7NQYkuUsPlZxBoMl',
+		      scope: ['profile', 'email']
+		    }
+	  }
+	}
 });

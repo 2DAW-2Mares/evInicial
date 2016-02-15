288187356b5cc3ec0994caff45650cce41800a40
Alberto Sierra <alberto.sierra@murciaeduca.es>
2016-02-15 12:23:53 +0100

    mejorando el log cuando se asocian roles a usuarios

--- api/models/User.js
+++ api/models/User.js
@@ -47,7 +47,7 @@ afterCreate: [
         .populate('user')
         .then(function (profesor) {
           if(profesor) {
-            Profesor.update(profesor.id, {user: user.id}).then(function(profesor){
+            Profesor.update(profesor.id, {user: user.id}).then(function(profesores){
              Promise.bind({ }, User.findOne(user.id)
               .populate('roles')
               .then(function (user) {
@@ -79,7 +79,7 @@ afterCreate: [
       	.populate('user')
       	.then(function (alumno) {
       		if(alumno) {
-	      		Alumno.update(alumno.id, {user: user.id}).then(function(alumno){
+	      		Alumno.update(alumno.id, {user: user.id}).then(function(alumnos){
 			       Promise.bind({ }, User.findOne(user.id)
 			        .populate('roles')
 			        .then(function (user) {
